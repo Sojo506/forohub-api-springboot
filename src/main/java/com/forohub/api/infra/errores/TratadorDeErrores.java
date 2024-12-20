fe53128f71131efdebe946dcd7469c2414bd4627
Sojo506 <fsojodev@gmail.com>
2024-12-19 18:50:02 -0600

    add 404 error handler

--- src/main/java/com/forohub/api/infra/errores/TratadorDeErrores.java
+++ src/main/java/com/forohub/api/infra/errores/TratadorDeErrores.java
@@ -1,6 +1,7 @@
 package com.forohub.api.infra.errores;
 
 import com.forohub.api.domain.ValidacionException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarError400(MethodArgumentNotValidException e) {
